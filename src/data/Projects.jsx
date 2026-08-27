cbcddfa3a612584f43d14c0e85ed7220722d9989
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2026-08-27 14:06:38 -0300

    Adição de novos projetos, ordenação e organização do sistema

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,9 +1,59 @@
 const projects = [
   {
+    name: "IntegraPI",
+    urls: [
+      { name: "Web", url: "https://integrapi.projetointegrador.caf.ufv.br" },
+      {
+        name: "API",
+        url: "https://api-integrapi.projetointegrador.caf.ufv.br/api-docs",
+      },
+    ],
+    img: "/assets/integrapi.png",
+  },
+  {
+    name: "Caldeirão - Marketplace",
+    urls: [
+      {
+        name: "GitHub",
+        url: "https://github.com/SabrinaBruni28/Caldeirao-Marketplace-Web",
+      },
+      { name: "Web", url: "https://caldeirao.onrender.com" },
+    ],
+    img: "/assets/caldeirao.ico",
+  },
+  {
+    name: "Movies Platform",
+    urls: [
+      {
+        name: "GitHub",
+        url: "https://github.com/SabrinaBruni28/Movies-Platform",
+      },
+      {
+        name: "Web",
+        url: "https://sabrinabruni28.github.io/Movies-Platform",
+      },
+    ],
+    img: "/assets/moviesplatform.png",
+  },
+  {
+    name: "Gerenciador de Tarefas",
+    urls: [
+      {
+        name: "GitHub",
+        url: "https://github.com/SabrinaBruni28/Gerenciador-de-Tarefas",
+      },
+      {
+        name: "Web",
+        url: "https://sabrinabruni28.github.io/Gerenciador-de-Tarefas",
+      },
+    ],
+    img: "/assets/gerenciadortarefas.png",
+  },
+  {
     name: "Catout",
     urls: [
       { name: "GitHub", url: "https://github.com/SabrinaBruni28/Catout" },
-      { name: "Web", url: "https://sabrinabruni28.github.io/Catout/" },
+      { name: "Web", url: "https://sabrinabruni28.github.io/Catout" },
       {
         name: "Download (Linux)",
         url: "https://github.com/SabrinaBruni28/Catout/releases/latest/download/Catout-Linux.tar.gz",
@@ -19,7 +69,7 @@ const projects = [
     name: "MegaMania",
     urls: [
       { name: "GitHub", url: "https://github.com/SabrinaBruni28/MegaMania" },
-      { name: "Web", url: "https://sabrinabruni28.github.io/MegaMania/" },
+      { name: "Web", url: "https://sabrinabruni28.github.io/MegaMania" },
       {
         name: "Download (Linux)",
         url: "https://github.com/SabrinaBruni28/MegaMania/releases/latest/download/MegaMania-Linux.tar.gz",
@@ -32,13 +82,67 @@ const projects = [
     img: "/assets/megamania.png",
   },
   {
+    name: "Desafio Digital",
+    urls: [
+      {
+        name: "Web",
+        url: "http://tomcat.nupessc.caf.ufv.br/desafiodigital",
+      },
+      {
+        name: "Download (Jar)",
+        url: "https://drive.google.com/uc?export=download&id=117cbm5pGrfY1L1hG3LIUAQxJsVQujqRS",
+      },
+      {
+        name: "Download (Android)",
+        url: "https://drive.google.com/uc?export=download&id=1lmNKNXU8T7_sdVn0EhywOd4F0v2PS8-M",
+      },
+    ],
+    img: "/assets/desafiodigital.png",
+  },
+  {
+    name: "Jogo da Forca",
+    urls: [
+      {
+        name: "GitHub",
+        url: "https://github.com/SabrinaBruni28/JogoDaForca-2.0",
+      },
+      {
+        name: "Web",
+        url: "https://sabrinabruni28.github.io/JogoDaForca-2.0",
+      },
+    ],
+    img: "/assets/forca.png",
+  },
+  {
+    name: "Cavaleirinho Jonny: O Resgate de Binny",
+    urls: [
+      {
+        name: "GitHub",
+        url: "https://github.com/SabrinaBruni28/Cavaleirinho-Jonny-O-Resgate-de-Binny",
+      },
+      {
+        name: "Web",
+        url: "https://sabrinabruni28.github.io/Cavaleirinho-Jonny-O-Resgate-de-Binny/",
+      },
+      {
+        name: "Download (Linux)",
+        url: "https://github.com/SabrinaBruni28/Cavaleirinho-Jonny-O-Resgate-de-Binny/releases/latest/download/Cavaleiro-Linux.tar.gz",
+      },
+      {
+        name: "Download (Windows)",
+        url: "https://github.com/SabrinaBruni28/Cavaleirinho-Jonny-O-Resgate-de-Binny/releases/latest/download/Cavaleiro-Windows.exe",
+      },
+    ],
+    img: "/assets/cavaleiro.png",
+  },
+  {
     name: "Jogo da Memória",
     urls: [
       {
         name: "GitHub",
         url: "https://github.com/SabrinaBruni28/JogoDaMemoria",
       },
-      { name: "Web", url: "https://sabrinabruni28.github.io/JogoDaMemoria/" },
+      { name: "Web", url: "https://sabrinabruni28.github.io/JogoDaMemoria" },
       {
         name: "Download (Jar)",
         url: "https://github.com/SabrinaBruni28/JogoDaMemoria/releases/latest/download/MemoriaGame-1.0.0.jar",
@@ -50,66 +154,13 @@ const projects = [
     name: "Snake Game",
     urls: [
       { name: "GitHub", url: "https://github.com/SabrinaBruni28/Snake-Game" },
-      { name: "Web", url: "https://sabrinabruni28.github.io/Snake-Game/" },
+      { name: "Web", url: "https://sabrinabruni28.github.io/Snake-Game" },
       {
         name: "Download (Jar)",
         url: "https://github.com/SabrinaBruni28/Snake-Game/releases/latest/download/SnakeGame-1.0.0.jar",
       },
     ],
     img: "/assets/snakegame.png",
-  },
-  {
-    name: "Gerenciador de Tarefas",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/Gerenciador-de-Tarefas",
-      },
-      {
-        name: "Web",
-        url: "https://sabrinabruni28.github.io/Gerenciador-de-Tarefas/",
-      },
-    ],
-    img: "/assets/gerenciadortarefas.png",
-  },
-  {
-    name: "Caldeirão - Marketplace",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/Caldeirao-Marketplace-Web",
-      },
-      { name: "Web", url: "https://caldeirao.onrender.com/" },
-    ],
-    img: "/assets/caldeirao.ico",
-  },
-  {
-    name: "Gravity Falls - Blog",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/GravityFalls",
-      },
-      {
-        name: "Web",
-        url: "https://sabrinabruni28.github.io/GravityFalls/",
-      },
-    ],
-    img: "/assets/gravityfalls.png",
-  },
-  {
-    name: "Movies Platform",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/Movies-Platform",
-      },
-      {
-        name: "Web",
-        url: "https://sabrinabruni28.github.io/Movies-Platform/",
-      },
-    ],
-    img: "/assets/moviesplatform.png",
   },
   {
     name: "Jogo da Velha",
@@ -174,60 +225,6 @@ const projects = [
       },
     ],
     img: "/assets/guitarriro.webp",
-  },
-  {
-    name: "Desafio Digital",
-    urls: [
-      {
-        name: "Web",
-        url: "http://tomcat.nupessc.caf.ufv.br/desafiodigital/",
-      },
-      {
-        name: "Download (Jar)",
-        url: "https://drive.google.com/uc?export=download&id=117cbm5pGrfY1L1hG3LIUAQxJsVQujqRS",
-      },
-      {
-        name: "Download (Android)",
-        url: "https://drive.google.com/uc?export=download&id=1lmNKNXU8T7_sdVn0EhywOd4F0v2PS8-M",
-      },
-    ],
-    img: "/assets/desafiodigital.png",
-  },
-  {
-    name: "Jogo da Forca",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/JogoDaForca-2.0",
-      },
-      {
-        name: "Web",
-        url: "https://sabrinabruni28.github.io/JogoDaForca-2.0/",
-      },
-    ],
-    img: "/assets/forca.png",
-  },
-  {
-    name: "Cavaleiro da Floresta",
-    urls: [
-      {
-        name: "GitHub",
-        url: "https://github.com/SabrinaBruni28/Cavaleiro",
-      },
-      {
-        name: "Web",
-        url: "https://sabrinabruni28.github.io/Cavaleiro/",
-      },
-      {
-        name: "Download (Linux)",
-        url: "https://github.com/SabrinaBruni28/Cavaleiro/releases/latest/download/Cavaleiro-Linux.tar.gz",
-      },
-      {
-        name: "Download (Windows)",
-        url: "https://github.com/SabrinaBruni28/Cavaleiro/releases/latest/download/Cavaleiro-Windows.exe",
-      },
-    ],
-    img: "/assets/cavaleiro.png",
   },
 ].map((project, index) => ({
   ...project,
